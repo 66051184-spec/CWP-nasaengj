b0bcfc39650d6ecd8a746d84873003194728d8d2
66051184 <66051184@kmitl.ac.th>
2026-09-24 10:09:41 +0700

    cell05

---
+++ cell06/ex05/scope_that.py
@@ -0,0 +1,10 @@
+def add_one(number):
+    number = number + 1
+    print("Inside add_one, number is now:", number)
+
+my_variable = 5
+print(my_variable)
+
+add_one(my_variable)
+
+print(my_variable)
